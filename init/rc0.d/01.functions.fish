b71dee862f46e07ec127a96105e11a4962b60901
Andrew Matthews <andrew.matthews@team.telstra.com.au>
2019-05-27 15:27:42 +1000

    --wip-- [skip ci]

--- init/rc0.d/01.functions.fish
+++ init/rc0.d/01.functions.fish
@@ -19,12 +19,12 @@ function _get -a varName key -d 'get property value from JSON formatted property
   echo $$varName | jq ".$key"
 end
 
-function create_msg_task -a title pri pro
-  test -z pri; or set pri "M"
-  test -z pro; or set pro $CURRENT_PROJECT_SN
-  set -g r '{"event-type": "task-new"}'
+function create_msg_task -a title pro pri 
+  set priority "$pri"; test -z "$priority"; and set priority "M"
+  set project "$pro"; test -z "$project"; and set project $CURRENT_PROJECT_SN
+  set -g r '{"event_type": "task-new"}'
   _set r title $title
-  _set r project $pro
-  _set r priority $pri
+  _set r project $project
+  _set r priority $priority
   echo $r
 end
