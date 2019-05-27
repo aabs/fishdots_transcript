5d2600cc7d3a2ee89232fd8775430a174c4bc448
Andrew Matthews <andrew.matthews@team.telstra.com.au>
2019-05-27 15:05:38 +1000

    --wip-- [skip ci]

--- init/rc0.d/01.functions.fish
+++ init/rc0.d/01.functions.fish
@@ -8,3 +8,23 @@ function x -d 'dispatcher'
   end
 end
 
+function _set -a varName key value -d 'add property to JSON formatted property set using JQ'
+  if test -z $varName
+    set -g $varName "{}"
+  end
+  set -g $varName (echo $$varName | jq ".$key |= \"$value\"")
+end
+
+function _get -a varName key -d 'get property value from JSON formatted property set using JQ'
+  echo $$varName | jq ".$key"
+end
+
+function create_msg_task -a title pri pro
+  test -z pri; or set pri "M"
+  test -z pro; or set pro $CURRENT_PROJECT_SN
+  set -g r '{"event-type": "task-new"}'
+  _set r title $title
+  _set r project $pro
+  _set r priority $pri
+  echo $r
+end
